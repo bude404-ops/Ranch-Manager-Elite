93edf55931f1da08799b1a174672a14b566912da
bude404-ops <bude404@gmail.com>
2026-06-06 11:39:53 -0700

    Create gps.js

---
+++ js/gps.js
@@ -0,0 +1,17 @@
+function addFence() {
+
+    alert("Fence feature coming soon");
+
+}
+
+function addWaterSource() {
+
+    alert("Water source feature coming soon");
+
+}
+
+function addGate() {
+
+    alert("Gate feature coming soon");
+
+}
