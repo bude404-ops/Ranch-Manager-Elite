5e57351a6fffe4dc19abe37c05796fe2b5dfdb97
bude404-ops <bude404@gmail.com>
2026-06-06 01:04:42 -0700

    Delete js/storage.js

--- js/storage.js
+++
@@ -1,8 +0,0 @@
-const Storage = {
-    get(key){
-        return JSON.parse(localStorage.getItem(key) || "[]");
-    },
-    set(key,value){
-        localStorage.setItem(key, JSON.stringify(value));
-    }
-};
