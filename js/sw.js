077d243e5d0ee645dd42e2ed12dd39bba3fb78d0
bude404-ops <bude404@gmail.com>
2026-06-05 23:15:09 -0700

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,22 @@
+const CACHE = "ranch-v1";
+
+const FILES = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/js/app.js",
+  "/js/storage.js",
+  "/js/charts.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
